ef01730d447ebd571fab8c403d0ae27ad7150d8f
vitorfaglioni@gmail.com <vitorfaglioni@gmail.com>
2024-03-29 13:00:59 -0300

    astro.config.mjs github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://vitorfaglioni.github.com',
+	base: '/dentro-da-minha-cabeca',
+	integrations: [mdx(), sitemap()]
 });
