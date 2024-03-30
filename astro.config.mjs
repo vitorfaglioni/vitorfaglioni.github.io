50df74aa9b23142f37a930664be9372fcc1f0610
vitorfaglioni@gmail.com <vitorfaglioni@gmail.com>
2024-03-30 08:28:40 -0300

    trying to solve domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://dentrodaminhacabeca.com.br',
-	base: '/dentro-da-minha-cabeca',
 	integrations: [mdx(), sitemap()]
 });
