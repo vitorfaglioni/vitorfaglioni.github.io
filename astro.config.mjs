17b2c0841ec936064801f9688b45b2f7deed00eb
vitorfaglioni@gmail.com <vitorfaglioni@gmail.com>
2024-03-30 08:23:06 -0300

    change site on astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://vitorfaglioni.github.com',
+	site: 'https://dentrodaminhacabeca.com.br',
 	base: '/dentro-da-minha-cabeca',
 	integrations: [mdx(), sitemap()]
 });
