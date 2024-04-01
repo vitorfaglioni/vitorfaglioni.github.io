ba36e303b57c13a47ef1d3ae4c3ce7e3bf7496a0
vitorfaglioni@gmail.com <vitorfaglioni@gmail.com>
2024-04-01 18:39:59 -0300

    trying to make peladananet.com.br work

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://vitorfaglioni.github.io',
-	base: '/pelada-na-net',
+	site: 'https://peladananet.com.br',
 	integrations: [mdx(), sitemap()]
 });
