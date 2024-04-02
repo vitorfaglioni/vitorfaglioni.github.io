cca51a89de6d3c2210df0635f6380738570193a9
vitorfaglioni@gmail.com <vitorfaglioni@gmail.com>
2024-04-02 08:47:34 -0300

    fixed site ref on astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://vitorfaglioni.github.io/',
+	site: 'https://vitorfaglioni.github.io',
 	integrations: [mdx(), sitemap()]
 });
